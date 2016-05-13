fa79fbfd6216cd707c05fa3098c060264a0cb4d9
Filippo Raimondi <filippo.raimondi@essencedigital.com>
2016-05-13 15:43:30 +0100

    implemented home page objects

--- lib/pages/home_page.rb
+++ lib/pages/home_page.rb
@@ -1,9 +1,22 @@
 module Pages
   class HomePage < Pages::BasePage
-    element(:test) { 'test' }
+    element(:search) { '#global-search' }
+    element(:search_go) { '#goButton' }
+    element(:search_result_grid) { '#product-listing > div > form > ol' }
+    element(:search_message) { '#product-listing-page > div.wrap.search-results-plp > div.M083.search-results' }
 
     def visit_page
-      visit 'http://www.test'
+      visit 'http://www.marksandspencer.com/'
+    end
+
+    def search_for_product(product)
+      search.set product
+      search_go.click
+    end
+
+    def product_count
+      products = search_result_grid.all 'li'
+      products.count
     end
   end
 end
